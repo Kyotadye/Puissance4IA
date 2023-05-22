a191af277526d3b373fea4fc196759fc8faa5ffe
p1911890 <anthony.gonzalez@etu.univ-lyon1.fr>
2023-05-22 15:35:54 +0200

    en classe

--- main.py
+++ main.py
@@ -1,96 +1,82 @@
 import numpy as np
 
-# Définition des constantes
-TAILLE_GRILLE = (6, 7)  # Taille de la grille de jeu
+class Puissance4:
+    def __init__(self):
+        self.taille_grille = (6, 7)
+        self.grille = np.zeros(self.taille_grille, dtype=int)
+        self.joueur_actuel = 1
+        self.nb_coups = 0
+        self.fin_jeu = False
 
-# Création de la grille vide
-grille = np.zeros(TAILLE_GRILLE, dtype=int)
-
-
-# Fonction pour afficher la grille
-def afficher_grille(grille):
-    for row in grille:
-        for cell in row:
-            if cell == 0:
-                print(".", end=" ")
-            elif cell == 1:
-                print("X", end=" ")
-            else:
-                print("O", end=" ")
+    def afficher_grille(self):
+        for row in self.grille:
+            for cell in row:
+                if cell == 0:
+                    print(".", end=" ")
+                elif cell == 1:
+                    print("X", end=" ")
+                else:
+                    print("O", end=" ")
+            print()
         print()
-    print()
 
+    def placer_jeton(self, colonne):
+        for i in range(self.taille_grille[0]-1, -1, -1):
+            if self.grille[i][colonne] == 0:
+                self.grille[i][colonne] = self.joueur_actuel
+                break
 
-# Fonction pour placer un jeton dans la colonne choisie
-def placer_jeton(grille, colonne, joueur):
-    for i in range(TAILLE_GRILLE[0] - 1, -1, -1):
-        if grille[i][colonne] == 0:
-            grille[i][colonne] = joueur
-            break
+    def verif_victoire(self):
+        joueur = self.joueur_actuel
 
+        # Vérification des lignes
+        for i in range(self.taille_grille[0]):
+            for j in range(self.taille_grille[1]-3):
+                if np.all(self.grille[i, j:j+4] == joueur):
+                    return True
 
-# Fonction pour vérifier si un joueur a gagné
-def verif_victoire(grille, joueur):
-    # Vérification des lignes
-    for i in range(TAILLE_GRILLE[0]):
-        for j in range(TAILLE_GRILLE[1] - 3):
-            if np.all(grille[i, j:j + 4] == joueur):
-                return True
+        # Vérification des colonnes
+        for i in range(self.taille_grille[0]-3):
+            for j in range(self.taille_grille[1]):
+                if np.all(self.grille[i:i+4, j] == joueur):
+                    return True
 
-    # Vérification des colonnes
-    for i in range(TAILLE_GRILLE[0] - 3):
-        for j in range(TAILLE_GRILLE[1]):
-            if np.all(grille[i:i + 4, j] == joueur):
-                return True
+        # Vérification des diagonales ascendantes
+        for i in range(self.taille_grille[0]-3):
+            for j in range(self.taille_grille[1]-3):
+                if np.all(np.diag(self.grille[i:i+4, j:j+4]) == joueur):
+                    return True
 
-    # Vérification des diagonales ascendantes
-    for i in range(TAILLE_GRILLE[0] - 3):
-        for j in range(TAILLE_GRILLE[1] - 3):
-            if np.all(np.diag(grille[i:i + 4, j:j + 4]) == joueur):
-                return True
+        # Vérification des diagonales descendantes
+        for i in range(self.taille_grille[0]-3):
+            for j in range(self.taille_grille[1]-3):
+                if np.all(np.diag(np.fliplr(self.grille[i:i+4, j:j+4])) == joueur):
+                    return True
 
-    # Vérification des diagonales descendantes
-    for i in range(TAILLE_GRILLE[0] - 3):
-        for j in range(TAILLE_GRILLE[1] - 3):
-            if np.all(np.diag(np.fliplr(grille[i:i + 4, j:j + 4])) == joueur):
-                return True
+        return False
 
-    return False
+    def jouer_puissance4(self):
+        while not self.fin_jeu:
+            self.afficher_grille()
 
+            # Demande à l'utilisateur de choisir la colonne
+            colonne = int(input(f"Joueur {self.joueur_actuel}, choisissez la colonne (0-6) : "))
 
-# Fonction principale pour jouer au Puissance 4
-def jouer_puissance4():
-    joueur_actuel = 1
-    nb_coups = 0
-    fin_jeu = False
+            self.placer_jeton(colonne)
+            self.nb_coups += 1
 
-    while not fin_jeu:
-        afficher_grille(grille)
+            if self.verif_victoire():
+                self.afficher_grille()
+                print(f"Joueur {self.joueur_actuel} a gagné !")
+                self.fin_jeu = True
+            elif self.nb_coups == self.taille_grille[0] * self.taille_grille[1]:
+                self.afficher_grille()
+                print("Match nul !")
+                self.fin_jeu = True
 
-        # Demande à l'utilisateur de choisir la colonne
-        if joueur_actuel == 1:
-            colonne = int(input("Joueur 1, choisissez la colonne (0-6) : "))
-        else:
-            colonne = int(input("Joueur 2, choisissez la colonne (0-6) : "))
-
-        placer_jeton(grille, colonne, joueur_actuel)
-        nb_coups += 1
-
-        if verif_victoire(grille, joueur_actuel):
-            afficher_grille(grille)
-            if joueur_actuel == 1:
-                print("Joueur 1 a gagné !")
-            else:
-                print("Joueur 2 a gagné !")
-            fin_jeu = True
-        elif nb_coups == TAILLE_GRILLE[0] * TAILLE_GRILLE[1]:
-            afficher_grille(grille)
-            print("Match nul !")
-            fin_jeu = True
-
-        # Changer de joueur
-        joueur_actuel = 3 - joueur_actuel
-
+            # Changer de joueur
+            self.joueur_actuel = 3 - self.joueur_actuel
 
 # Lancer le jeu
-jouer_puissance4()
+jeu = Puissance4()
+jeu.jouer_puissance4()
